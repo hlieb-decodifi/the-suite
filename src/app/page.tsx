8523b607ce217f233ff696b0d8700ec865ff039d
Hlieb Shchehelskyi <hliebshchehelskyi@calls9.com>
2025-04-29 12:12:50 +0200

    Refactor HomePage to simplify rendering by removing unnecessary elements. Update SignUpModal to streamline the submission process by removing conditional rendering for submission success, enhancing clarity and maintainability.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,5 @@
 import { HomeTemplate } from '@/components/templates/HomeTemplate';
 
 export default function HomePage() {
-  return (
-    <>
-      <HomeTemplate />
-      <div className="hidden">
-        {`${process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:3000'}/auth/callback`}
-      </div>
-    </>
-  );
+  return <HomeTemplate />;
 }
