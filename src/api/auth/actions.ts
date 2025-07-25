bf6752fd644cedb2229870fd08561b6c9c130d61
Hlieb Shchehelskyi <hlieb@decodifi.uk>
2025-07-25 17:30:46 +0200

    Update URL retrieval logic in auth actions to correct environment variable references
    - Changed the reference from VERCEL_BRANCH_URL to VERCEL_URL for improved accuracy in URL retrieval.
    - Enhanced logging to include the VERCEL_BRANCH_URL for better debugging and visibility.
    - Maintained the fallback to localhost URL for local development.

--- src/api/auth/actions.ts
+++ src/api/auth/actions.ts
@@ -9,13 +9,14 @@ import { createClient as createAdminClient } from '@supabase/supabase-js';
 
 const getURL = () => {
   let url =
-    process?.env?.VERCEL_BRANCH_URL ?? // Automatically set by Vercel.
+    process?.env?.VERCEL_URL ?? // Automatically set by Vercel.
     process?.env?.NEXT_PUBLIC_BASE_URL ?? // Set this to your site URL in production env.
     'http://localhost:3000/'
   // Make sure to include `https://` when not localhost.
   console.log('url', url);
   console.log('process.env.NEXT_PUBLIC_BASE_URL', process.env.NEXT_PUBLIC_BASE_URL);
   console.log('process.env.VERCEL_URL', process.env.NEXT_PUBLIC_VERCEL_URL);
+  console.log('process.env.VERCEL_BRANCH_URL', process.env.VERCEL_BRANCH_URL);
   console.log('process.env.NEXT_PUBLIC_SITE_URL', process.env.NEXT_PUBLIC_SITE_URL);
   url = url.startsWith('http') ? url : `https://${url}`
   // Make sure to include a trailing `/`.
